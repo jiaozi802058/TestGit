170a23dffbf1d507303209ba0d2ea6730e1f6732
jyingj <1156088591@qq.com>
2017-02-27 23:52:12 +0800

    05

--- src/com/jiaozi/TestGit.java
+++ src/com/jiaozi/TestGit.java
@@ -4,7 +4,6 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("厉害了Word哥");
 		System.out.println("大神驾到");
 		System.out.println("厉害了Word哥");
 		System.out.println("厉害了Word哥1");
