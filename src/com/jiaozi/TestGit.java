3ca91b65d0752f70e2bd0057ebf6542b73694e46
jiaozi <1114047382@qq.com>
2017-02-27 23:43:15 +0800

    04

--- src/com/jiaozi/TestGit.java
+++ src/com/jiaozi/TestGit.java
@@ -4,6 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("大神驾到");
 		System.out.println("厉害了Word哥");
 		System.out.println("厉害了Word哥1");
 		System.out.println("厉害了Word哥2");
