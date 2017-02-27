054e4ff3855d6246e8955ea20a2e9ec1cbc895fc
jiaozi <1114047382@qq.com>
2017-02-27 23:40:51 +0800

    04

--- src/com/jiaozi/TestGit.java
+++ src/com/jiaozi/TestGit.java
@@ -4,7 +4,12 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("大神驾到");
+		System.out.println("厉害了Word哥");
+		System.out.println("厉害了Word哥1");
+		System.out.println("厉害了Word哥2");
+		System.out.println("厉害了Word哥3");
+		System.out.println("厉害了Word哥4");
+		System.out.println("厉害了Word哥5");
 	}
 
 }
