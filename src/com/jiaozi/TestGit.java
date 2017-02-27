a1aff98587b8db702b37d0ffaa6a62008b931b94
jyingj <1156088591@qq.com>
2017-02-27 23:54:46 +0800

    06

--- src/com/jiaozi/TestGit.java
+++ src/com/jiaozi/TestGit.java
@@ -11,6 +11,9 @@ public class TestGit {
 		System.out.println("厉害了Word哥3");
 		System.out.println("厉害了Word哥4");
 		System.out.println("厉害了Word哥5");
+		System.out.println("厉害了Word哥6");
+		System.out.println("厉害了Word哥7");
+		System.out.println("厉害了Word哥8");
 	}
 
 }
