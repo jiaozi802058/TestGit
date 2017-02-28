c5183f2d73df0ed46f2e61259d632f30e7f7eef0
jiaozi802058 <jiaozi19880205>
2017-02-28 08:37:33 +0800

    07

--- src/com/jiaozi/TestGit.java
+++ src/com/jiaozi/TestGit.java
@@ -4,15 +4,13 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
-		System.out.println("大神驾到");
+		System.out.println("雷神驾到");
 		System.out.println("厉害了Word哥");
 		System.out.println("厉害了Word哥1");
 		System.out.println("厉害了Word哥2");
 		System.out.println("厉害了Word哥3");
 		System.out.println("厉害了Word哥4");
 		System.out.println("厉害了Word哥5");
-		System.out.println("hahahha");
-		System.out.println(11111);
 		System.out.println("厉害了Word哥6");
 		System.out.println("厉害了Word哥7");
 		System.out.println("厉害了Word哥8");
